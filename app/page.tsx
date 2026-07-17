e8ffa87182ee885ce59d8796d330e4adfb51f93b
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-07-17 12:12:54 +0600

    TrustBadge Section Updated

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,8 @@ export default function Home() {
       <ProductGrid/>
       <TrustBadges />
       <DealsOfTheDay/>
-      <PromoStrip/>
       <AppPromo/>
+      <PromoStrip/>
       <Footer/>
     </main>
   );
