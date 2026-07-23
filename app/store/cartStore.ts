1e99cda6d123fd9778c18564d782e5a3604adc2d
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-07-24 00:16:17 +0600

    CartStore Section Updated

--- app/store/cartStore.ts
+++ app/store/cartStore.ts
@@ -9,4 +9,15 @@ discount?: number| null;
 imageUrl: string;
 stock: number;
 quantity: number;
-}
+};
+
+type CartStore = {
+   items: CartItem[];
+   addItem: (item: Omit<CartItem, "quantity">, qty?: number) => void;
+   removeItem: (id: string) => void;
+   increaseQty: (id: string) => void;
+   decreaseQty: (id: string) => void;
+   clearCart: () => void;
+   totalIems: () => number;
+   totalPrice: () => number;
+};
