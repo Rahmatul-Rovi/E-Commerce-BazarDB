0facfa3fa2607bd2f6c9bb1c8e53cb4134ecd0c4
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-08-04 00:17:28 +0600

    Dashboard order section updated

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -6,3 +6,14 @@ type OrderItem = {
     price: number;
     product: { name: string; imageUrl: string; slug: string };
 };
+
+type Order = {
+  id: string;
+  total: number;
+  status: string;
+  createdAt: string;
+  address: string;
+  city: string;
+  paymentMethod: string;
+  items: OrderItem[];
+};
