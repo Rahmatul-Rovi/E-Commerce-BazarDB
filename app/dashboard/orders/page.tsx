8f90d0a5d857d5a05c6139ed982511837cc0d895
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-08-04 10:15:13 +0600

    Orders Page Section Updated

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+
 type OrderItem = {
     id: string;
     quantity: number;
@@ -17,3 +19,8 @@ type Order = {
   paymentMethod: string;
   items: OrderItem[];
 };
+
+export default function MyOrdersPage() {
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [loading, setLoading] = useState(true);
+}
