718731b16d22f52c11c3d9640371b73bd00bee1c
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-08-07 11:51:56 +0600

    Dashboard Order Section Updated

--- app/dashboard/orders/[id]/page.tsx
+++ app/dashboard/orders/[id]/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { useParams } from "next/navigation";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
 type OrderItem = {
     id: string;
     quantity: number;
@@ -21,3 +25,20 @@ type Order = {
 };
 
 const statusSteps = ["pending", "processing", "delivered"];
+
+export default function OrderDetailsPage() {
+    const params = useParams();
+    const router = useRouter();
+    const [order , setOrder] = useState<Order | null>(null);
+    const [loading , setLoading] = useState(true);
+
+    useEffect(()=> {
+        fetch("/api/my-orders")
+        .then((res)=> res.json())
+        .then((data : Order[]) => {
+            const found = data.find((o) => o.id === params.id);
+        setOrder(found || null);
+        setLoading(false);
+        });
+    }, [params.id]);
+}
