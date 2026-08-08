e3d70d6e9f6306d97fd162702172f9b3fa1bb806
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-08-09 00:12:13 +0600

    Admin page Section Updated

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -11,4 +11,26 @@ export default async function AdminOverview() {
       include: { user: true },
         }),
     ]);
+
+     const totalRevenue = await prisma.order.aggregate({
+    _sum: { total: true },
+  });
+
+  return(
+    <div>
+         <h1 className="font-heading text-2xl font-bold text-gray-900 mb-1">
+        Admin Overview
+      </h1>
+      <p className="text-gray-500 text-sm mb-6">Store performance at a glance.</p>
+
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
+        <div className="bg-white rounded-2xl p-5 border border-gray-100">
+          <DollarSign size={20} className="text-primary mb-2" />
+          <p className="text-2xl font-heading font-bold text-gray-900">
+            ৳{(totalRevenue._sum.total || 0).toFixed(0)}
+          </p>
+          <p className="text-xs text-gray-500">Total Revenue</p>
+        </div>
+    </div>
+  )
 }
