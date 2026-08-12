9821b18ddc55110b229bb219710a980d804b4c8d
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-08-12 20:02:45 +0600

    Admin Product Route Updated

--- app/api/admin/products/[id]/route.ts
+++ app/api/admin/products/[id]/route.ts
@@ -1,4 +1,5 @@
 import { auth } from "@/auth";
+import { NextResponse } from "next/server";
 
 async function checkAdmin() {
     const session = await auth();
@@ -9,5 +10,10 @@ export async function PATCH(
     request: Request,
     {params}: {params: Promise<{id: string}>}
 ){
-    
+    const session = await checkAdmin();
+    if (!session) return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+
+    const {id} = await params;
+    const body = await request.json();
+    const { name, slug, price, discount, imageUrl, stock, categoryId } = body;
 }
