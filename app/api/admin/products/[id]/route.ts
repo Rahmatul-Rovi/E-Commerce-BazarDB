aad2325d2fc525690475c8df8dd5b9fc93964e96
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-08-12 19:58:07 +0600

    Admin Product Section Updated

--- app/api/admin/products/[id]/route.ts
+++ app/api/admin/products/[id]/route.ts
@@ -4,3 +4,10 @@ async function checkAdmin() {
     const session = await auth();
     return session?.user?.role === "admin" ? session : null;
 }
+
+export async function PATCH(
+    request: Request,
+    {params}: {params: Promise<{id: string}>}
+){
+    
+}
