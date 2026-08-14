7b7e1ce256f5706146040135e9356316e287caca
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-08-14 15:55:23 +0600

    Admin Product Section Updated

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type Product = {
   id: string;
   name: string;
@@ -9,3 +11,17 @@ type Product = {
   category: { name: string };
 };
 
+export default function AdminProductsPage() {
+    const [products , setProducts] = useState<Product[]>([]);
+    const [loading , setLoading] = useState(true);
+
+    const loadProducts = () => {
+        fetch("/api/admin/products")
+        .then((res)=> res.json())
+        .then((data) => {
+            setProducts(Array.isArray(data) ? data : []);
+        setLoading(false);
+        });
+    };
+    
+}
