5e130a9619412879deaaf3c18241d418c44c7bc6
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-08-14 15:50:12 +0600

    Admin Product Section Updated

---
+++ app/admin/products/page.tsx
@@ -0,0 +1,11 @@
+type Product = {
+  id: string;
+  name: string;
+  slug: string;
+  price: number;
+  discount?: number | null;
+  imageUrl: string;
+  stock: number;
+  category: { name: string };
+};
+
