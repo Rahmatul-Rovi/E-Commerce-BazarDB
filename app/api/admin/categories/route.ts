e8a5e0c50fd33073ec79ad47372f21953fbb92ba
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-08-18 11:11:54 +0600

    Admin Categories Route Updated

--- app/api/admin/categories/route.ts
+++ app/api/admin/categories/route.ts
@@ -1,10 +1,15 @@
 import { auth } from "@/auth";
-import { error } from "console";
 import { NextResponse } from "next/server";
 
 export async function POST(request : Request) {
     const session = await auth();
     if(session?.user?.role !== "admin"){
         return NextResponse.json({error: "Forbidded"}, {status:403});
+    }
+
+    const {name , slug} = await request.json();
+
+    if(!name || !slug) {
+        return NextResponse.json({error: "Name and Slug Required"}, {status:400});
     }
 }
