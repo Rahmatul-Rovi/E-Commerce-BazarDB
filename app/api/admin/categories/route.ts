69a6cfd62f8a14758e9945d35fc5146ca3ade6ea
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-08-19 11:22:33 +0600

    Admin Category Route Updated

--- app/api/admin/categories/route.ts
+++ app/api/admin/categories/route.ts
@@ -1,4 +1,5 @@
 import { auth } from "@/auth";
+import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function POST(request : Request) {
@@ -11,5 +12,12 @@ export async function POST(request : Request) {
 
     if(!name || !slug) {
         return NextResponse.json({error: "Name and Slug Required"}, {status:400});
+    }
+
+    try{
+        const category = await prisma.category.create({data: { name, slug }});
+        return NextResponse.json(category, { status:201 });
+    } catch {
+        return NextResponse.json({ error: "Slug already exists" }, { status: 409 });
     }
 }
