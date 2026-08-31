0d9086370d7bd811571c419aa06f5e88ae970c87
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-08-31 11:16:24 +0600

    Admin Orders Route Updated

--- app/api/admin/orders/[id]/route.ts
+++ app/api/admin/orders/[id]/route.ts
@@ -1,21 +1,23 @@
-import { auth } from "@/auth";
-import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { auth } from "@/auth";
 
 export async function PATCH(
-    request: Request,
-    { params }: { params: Promise<{ id: string }> }
-){
-    const session = await auth();
-    if (session?.user?.role !== "admin") {
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const session = await auth();
+  if (session?.user?.role !== "admin") {
     return NextResponse.json({ error: "Forbidden" }, { status: 403 });
   }
 
-  const {id} = await params;
-  const {status} = await request.json();
+  const { id } = await params;
+  const { status } = await request.json();
 
   const order = await prisma.order.update({
-    where: {id},
-    data: {status},
+    where: { id },
+    data: { status },
   });
+
+  return NextResponse.json(order);
 }
