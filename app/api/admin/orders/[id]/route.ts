3e2ec3b93f590962214c67a0badc447b00f09ab4
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-08-31 11:14:26 +0600

    Admin Order Route Updated

--- app/api/admin/orders/[id]/route.ts
+++ app/api/admin/orders/[id]/route.ts
@@ -1,4 +1,15 @@
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+
 export async function PATCH(
     request: Request,
     { params }: { params: Promise<{ id: string }> }
-)
+){
+    const session = await auth();
+    if (session?.user?.role !== "admin") {
+    return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+  }
+
+  const {id} = await params;
+  const {status} = await request.json();
+}
