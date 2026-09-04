06f5b82c9bd62a9ac52f66e398daa09c828c6652
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-09-05 00:18:38 +0600

    Admin Users Route Updated

--- app/api/admin/users/route.ts
+++ app/api/admin/users/route.ts
@@ -1,9 +1,18 @@
 import { auth } from "@/auth";
+import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(){
     const session = await auth();
     if(session?.user?.role !== "admin"){
-        return NextResponse.json()
+        return NextResponse.json({error: "Forbidden"}, {status: 403});
     }
+
+    const users = await prisma.user.findMany({
+        select: {
+            id: true,
+      name: true,
+      email: true,
+        }
+    })
 }
