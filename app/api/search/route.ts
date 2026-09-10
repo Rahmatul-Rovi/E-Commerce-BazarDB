93da5ec242385c29d4fe4d2b31f97832f960951a
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-09-11 00:09:58 +0600

    Search Route Section Updated

---
+++ app/api/search/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request){
+    const {searchParams} = new URL(request.url);
+    const query = searchParams.get("q")?.trim() || "";
+
+    if(!query){
+        return NextResponse.json([]);
+    }
+}
