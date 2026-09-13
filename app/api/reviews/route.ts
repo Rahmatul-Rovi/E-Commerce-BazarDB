789487e5dfc9eb89c884124aadea4e9e1d4bd01d
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-09-13 10:08:45 +0600

    Reviews Route Section Updated

--- app/api/reviews/route.ts
+++ app/api/reviews/route.ts
@@ -1,3 +1,4 @@
+import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
@@ -7,3 +8,9 @@ export async function GET(request: Request) {
   if (!productId) {
     return NextResponse.json({ error: "productId is required" }, { status: 400 });
   }
+
+  const reviews = await prisma.review.findMany({
+    where: {productId},
+    include: { user: { select: { name: true, image: true } } },
+    orderBy: { createdAt: "desc" },
+  });
