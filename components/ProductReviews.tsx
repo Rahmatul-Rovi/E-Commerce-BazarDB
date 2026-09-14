83d7a1a7edbd63e58dad3784faa98362855ec797
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-09-14 16:52:30 +0600

    Product Reviews Section Updated

--- components/ProductReviews.tsx
+++ components/ProductReviews.tsx
@@ -7,3 +7,15 @@ type Review = {
   createdAt: string;
   user: { name: string; image: string | null };
 };
+
+function StarRating({
+  rating,
+  size = 16,
+  interactive = false,
+  onChange,
+}: {
+  rating: number;
+  size?: number;
+  interactive?: boolean;
+  onChange?: (value: number) => void;
+}) {
