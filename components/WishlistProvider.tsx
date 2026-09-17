f6112890178537e02bda585cc172aca77632e52a
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-09-18 00:11:22 +0600

    Wishlist Provider Section Updated

--- components/WishlistProvider.tsx
+++ components/WishlistProvider.tsx
@@ -3,3 +3,15 @@
 export default function WishlistProvider({ children }: { children: React.ReactNode }) {
   const { data: session } = useSession();
   const setIds = useWishlistStore((state) => state.setIds);
+
+  useEffect(() => {
+    if (session?.user) {
+      fetch("/api/wishlist")
+        .then((res) => res.json())
+        .then((data) => {
+          if (Array.isArray(data)) {
+            setIds(data.map((w: { productId: string }) => w.productId));
+          }
+        });
+    } else {
+      setIds([]);
