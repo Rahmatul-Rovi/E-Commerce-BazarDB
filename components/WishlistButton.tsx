53bc4ee652440b2505c50c6f54af2efd7cff0a50
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-09-18 00:15:31 +0600

    Wishlist Button Section Updated

--- components/WishlistButton.tsx
+++ components/WishlistButton.tsx
@@ -4,4 +4,11 @@ export default function WishlistButton({
   productId,
   size = 16,
   className = "",
-}:
+}: {
+  productId: string;
+  size?: number;
+  className?: string;
+}) {
+  const { data: session } = useSession();
+  const { ids, toggle } = useWishlistStore();
+  const isWishlisted = ids.has(productId);
